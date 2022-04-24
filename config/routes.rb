a70e52e4248254dffe3310c3c8aaffd55cf65676
Burt <2902978956@qq.com>
2022-04-24 21:05:50 +0800

    Defines the root path route ("/")

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get '/', to: 'home#index'
+
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
